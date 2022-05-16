ca530f5e70739d14e8dca0142717983c127f6141
Michal Drwiega <michal.drwiega@robotec.ai>
2022-05-17 01:29:17 +0200

    Add draft of URDF to FBX conversion
    Signed-off-by: Michal Drwiega <michal.drwiega@robotec.ai>

--- Code/Source/URDF/UrdfToFbxConverter.cpp
+++ Code/Source/URDF/UrdfToFbxConverter.cpp
@@ -19,12 +19,46 @@ namespace ROS2
     {
         // TODO: Add implementation
 
-        // Workflow
         // 1. Parse URDF with UrdfParser
-        // 2. Create FBX file with FbxGenerator
-        // 3. Add each object from URDF based structure to FBX generator.
+        UrdfParser parser;
+        const auto urdf = parser.Parse(urdfString);
 
-        return "";
+        // 2. Create FBX file with FbxGenerator
+        Fbx::FbxGenerator generator;
+
+        // Add material
+        // TODO: add more materials
+        Fbx::Color color(0.0, 0.0, 0.0);
+        const auto materialId = generator.AddMaterial("black", color);
+
+        // 3. Add each object from URDF based structure to FBX generator
+        const auto root = urdf->getRoot();
+        
+        // TODO: Add depth first search - now it works only for depth = 1
+        // while (const auto link = root; !link->child_links.empty())
+        // {
+        // }
+        // Get link from URDF and add to FBX
+        const auto link = root; // TODO: just PoC
+        const auto linkName = link->name;
+        const auto linkGeometry = link->visual->geometry;
+        // TODO: handle different materials, link.visual.material
+
+        if (linkGeometry->type == urdf::Geometry::BOX)
+        {
+            auto boxGeometry = std::dynamic_pointer_cast<urdf::Box>(linkGeometry);
+            const double cubeSize = boxGeometry->dim.x; // TODO: Handle box in FBX instead of cube
+            const auto cubeId = generator.AddCubeObject(linkName, cubeSize, materialId);
+            (void)cubeId;
+        }
+        else
+        {
+            // Only box is supported now
+        }
+
+        // TODO: handle joints
+
+        return generator.GetFbxString();
     }
 
 } // namespace ROS2
